410bd7b71ee4f9f4ea5c408a20283e71a8ad0e75
Devanshchowdhury2212 <devanshchowdhury@gmail.com>
2023-08-01 19:58:19 +0530

    best staus

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -25,7 +25,7 @@ function ResumeNew() {
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href='https://drive.google.com/file/d/1gKHcyShkD7SNqgPZtZ255lPrJXwPopRB/view'
+            href='https://drive.google.com/file/d/1D6Aefs-ZG9TiQdIGCnqUD1o-3elsG-FS/view'
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
